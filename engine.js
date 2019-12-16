bee20c4f6785d0f4aecc0c18e17fb2070d88f1d1
Samy A Bencherif <sbenche2@ncsu.edu>
2019-12-15 22:38:14 -0500

    pop out feature

--- engine.js
+++ engine.js
@@ -6,5 +6,28 @@ const engine = {
         rightArrowIco: `<span class="boxedBtn">&#x21e8;</span>`,
         downArrowIco: `<span class="boxedBtn">&#x21e9;</span>`,
         getKeyBtnIco: (c) => `<span class="boxedBtn">${c}</span>`
+    },
+
+    saveGame: function()
+    {
+        console.warn("\"Save Game\" is not implemented.")
+    },
+
+    popOut: function () {
+        engine.saveGame()
+        document.querySelector("#startHintBox").classList.remove('hidden')
+        document.querySelector("#startHint").innerText = "Game is popped out."
+        var W = window.open(window.location.href + '?windowed=true','_blank','width=800,height=800')
+    },
+
+    enterWindowedMode: function() {
+        var gameView = document.querySelector("#gameView")
+
+        document.body.innerHTML = ""
+        document.body.appendChild(gameView)
+        document.body.style.margin = "0"
+
+        gameView.style.width = "100vw"
+        gameView.style.height = "100vh"
     }
 }
